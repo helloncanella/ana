b6a3b9408ddd509f0ee32900f2fb651b97414ca9
Hellon Canella <helloncanella@gmail.com>
2016-01-23 23:06:25 -0200

    final

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-'use strict';
-
 var gulp = require('gulp');
 var latex = require('gulp-latex');
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
 
 gulp.task('latex',function() {
   return gulp.src('*.tex')
     .pipe( latex() )
     .pipe( gulp.dest('./') );
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./*.tex', ['latex']);
 });
